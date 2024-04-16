edf5f4337230b966ab3c410482bfc9ef0729b985
Aditha Pathiraja <adithapathiraja@gmail.com>
2024-04-16 23:59:37 +0530

    added ability to log the curl responses

--- src/App/Services/Client/NgeniusClient.php
+++ src/App/Services/Client/NgeniusClient.php
@@ -9,7 +9,8 @@ use Jeybin\Networkintl\App\Models\NgeniusGateway;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Log;
 
-class NgeniusClient {
+class NgeniusClient
+{
 
     /**
      * Base api url of ngenius
@@ -18,8 +19,8 @@ class NgeniusClient {
      */
     private $BASE_URL;
 
-    
-        /**
+
+    /**
      * Base api url of ngenius
      *
      * @var [string]
@@ -42,7 +43,8 @@ class NgeniusClient {
     private $BEARER_TOKEN;
 
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->initalize();
     }
 
@@ -51,16 +53,17 @@ class NgeniusClient {
      * Variables
      * 
      */
-    private function initalize(){
+    private function initalize()
+    {
 
         /**
          * Fetching the gateway configurations from the
          * table ngenius_gateway and setting some of them
          * as private variables
          */
-        $gatewayConfig = NgeniusGateway::where('active',true)->first();
-        if(empty($gatewayConfig)){
-            throwNgeniusPackageResponse('Please configure ngenius_gateway table to continue',null,422);
+        $gatewayConfig = NgeniusGateway::where('active', true)->first();
+        if (empty($gatewayConfig)) {
+            throwNgeniusPackageResponse('Please configure ngenius_gateway table to continue', null, 422);
         }
 
         /**
@@ -80,18 +83,18 @@ class NgeniusClient {
          * the private variable. The bearer token will expire in 5 minutes
          */
         $this->BEARER_TOKEN = $this->ACCESS_TOKEN($gatewayConfig->api_key);
-
     }
 
-    protected function setApi($api='/'){
+    protected function setApi($api = '/')
+    {
         /**
          * If with reference api string contains
          * {outlet-reference} string it will be
          * replaced with the reference id from 
          * the settings table 
          */
-        $api = (strpos($api,'{outlet-reference}')) ? str_replace('{outlet-reference}',$this->REFERENCE_ID,$api) : $api;
-        $this->API_URL = $this->BASE_URL.$api;
+        $api = (strpos($api, '{outlet-reference}')) ? str_replace('{outlet-reference}', $this->REFERENCE_ID, $api) : $api;
+        $this->API_URL = $this->BASE_URL . $api;
         return $this;
     }
 
@@ -100,7 +103,8 @@ class NgeniusClient {
      * Using curl because the guzzle http client 
      * always giving Bad request error
      */
-    private function ACCESS_TOKEN($API_KEY){
+    private function ACCESS_TOKEN($API_KEY)
+    {
 
         try {
             /**
@@ -110,19 +114,36 @@ class NgeniusClient {
             /**
              * Api headers for the curl request
              */
-            $headers = array("authorization: Basic ".$API_KEY,
-                             "accept: application/vnd.ni-identity.v1+json",
-                             "content-type: application/vnd.ni-identity.v1+json");
-    
-            $ch = curl_init(); 
-            curl_setopt($ch, CURLOPT_URL, $this->API_URL); 
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
-            curl_setopt($ch, CURLOPT_POST, 1); 
-            $output = json_decode(curl_exec($ch));
-            if(!empty($output) && !empty($output->access_token)){
+            $headers = array(
+                "authorization: Basic " . $API_KEY,
+                "accept: application/vnd.ni-identity.v1+json",
+                "content-type: application/vnd.ni-identity.v1+json"
+            );
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->API_URL);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            if (config('ngenius-config.log_curl_info')) {
+                $verbose = fopen(storage_path('logs/laravel.log'), 'a');
+                curl_setopt($ch, CURLOPT_VERBOSE, true);
+                curl_setopt($ch, CURLOPT_STDERR, $verbose);
+            }
+
+            $res = curl_exec($ch);
+            $output = json_decode($res);
+
+            if (config('ngenius-config.log_curl_info')) {
+                Log::channel(config('ngenius-config.log_channel'))->info('Curl request executed successfully.', [
+                    'curl_info:' => $res,
+                ]);
+                fclose($verbose);
+            }
+
+            if (!empty($output) && !empty($output->access_token)) {
                 return $output->access_token;
-            }else{
+            } else {
                 throwNgeniusPackageResponse($output);
             }
         } catch (Exception $exception) {
@@ -131,110 +152,109 @@ class NgeniusClient {
     }
 
 
-    protected function execute($type,$request=[],$headers=[]){
+    protected function execute($type, $request = [], $headers = [])
+    {
         $type = strtolower($type);
-        if(!in_array($type,['post','get','put','delete'])){
-            throwNgeniusPackageResponse('Invalid execute type please check!',null,500);
+        if (!in_array($type, ['post', 'get', 'put', 'delete'])) {
+            throwNgeniusPackageResponse('Invalid execute type please check!', null, 500);
         }
 
-        if(empty($this->BEARER_TOKEN)){
-            throwNgeniusPackageResponse('Access token not found, please generate access token to continue',null,422);
+        if (empty($this->BEARER_TOKEN)) {
+            throwNgeniusPackageResponse('Access token not found, please generate access token to continue', null, 422);
         }
 
-        $allheaders = ['Content-Type' =>'application/vnd.ni-payment.v2+json',
-                       'Accept'       =>'application/vnd.ni-payment.v2+json'];
+        $allheaders = [
+            'Content-Type' => 'application/vnd.ni-payment.v2+json',
+            'Accept'       => 'application/vnd.ni-payment.v2+json'
+        ];
 
-        if(!empty($headers)){
-            $allheaders = array_merge($allheaders,$headers);
+        if (!empty($headers)) {
+            $allheaders = array_merge($allheaders, $headers);
         }
 
-        if($type == 'post'){
+        if ($type == 'post') {
             $response =  $this->POST_REQUEST($allheaders, $request);
         }
 
-        if($type == 'get'){
+        if ($type == 'get') {
             $response =  $this->GET_REQUEST($allheaders);
         }
 
-        if($type == 'put'){
-            $response =  $this->PUT_REQUEST($allheaders,$request);
+        if ($type == 'put') {
+            $response =  $this->PUT_REQUEST($allheaders, $request);
         }
 
-        if($type == 'delete'){
+        if ($type == 'delete') {
             $response =  $this->DELETE_REQUEST($allheaders);
         }
 
 
-        if(empty($response->json())){
-            throwNgeniusPackageResponse('Failed generate payment url',null,406);
+        if (empty($response->json())) {
+            throwNgeniusPackageResponse('Failed generate payment url', null, 406);
         }
 
-        if((!$response->successful())){
-            if(!empty($response['message']) && !empty($response['errors']) && !empty($response['code'])){
-                throwNgeniusPackageResponse($response['message'],$response['errors'],$response['code']);
+        if ((!$response->successful())) {
+            if (!empty($response['message']) && !empty($response['errors']) && !empty($response['code'])) {
+                throwNgeniusPackageResponse($response['message'], $response['errors'], $response['code']);
             }
             throwNgeniusPackageResponse($response);
         }
 
         return $response;
-
     }
 
-    private function POST_REQUEST($headers, $body=[]){
+    private function POST_REQUEST($headers, $body = [])
+    {
         try {
             return Http::withHeaders($headers)
-                            ->withToken($this->BEARER_TOKEN)
-                            ->post($this->API_URL,$body);
-
+                ->withToken($this->BEARER_TOKEN)
+                ->post($this->API_URL, $body);
         } catch (\Exception $exception) {
             throwNgeniusPackageResponse($exception);
-        }catch(ConnectionException $connException){
+        } catch (ConnectionException $connException) {
             throwNgeniusPackageResponse($connException);
         }
-
     }
 
 
-    private function GET_REQUEST($headers){
+    private function GET_REQUEST($headers)
+    {
         try {
-            $headers = ['Accept'       =>'application/vnd.ni-payment.v2+json'];
+            $headers = ['Accept'       => 'application/vnd.ni-payment.v2+json'];
             return Http::withHeaders($headers)
-                       ->withToken($this->BEARER_TOKEN)
-                       ->get($this->API_URL);
+                ->withToken($this->BEARER_TOKEN)
+                ->get($this->API_URL);
         } catch (Exception $exception) {
             throwNgeniusPackageResponse($exception);
-        }catch(ConnectionException $connException){
+        } catch (ConnectionException $connException) {
             throwNgeniusPackageResponse($connException);
         }
     }
 
-    private function PUT_REQUEST($headers,$body=[]){
+    private function PUT_REQUEST($headers, $body = [])
+    {
         try {
             return Http::withHeaders($headers)
-                            ->withToken($this->BEARER_TOKEN)
-                            ->put($this->API_URL,$body);
-
+                ->withToken($this->BEARER_TOKEN)
+                ->put($this->API_URL, $body);
         } catch (Exception $exception) {
             throwNgeniusPackageResponse($exception);
-        }catch(ConnectionException $connException){
+        } catch (ConnectionException $connException) {
             throwNgeniusPackageResponse($connException);
         }
     }
 
-    private function DELETE_REQUEST($headers){
+    private function DELETE_REQUEST($headers)
+    {
         try {
-            $headers = ['Accept'       =>'application/vnd.ni-payment.v2+json'];
+            $headers = ['Accept'       => 'application/vnd.ni-payment.v2+json'];
             return Http::withHeaders($headers)
-                       ->withToken($this->BEARER_TOKEN)
-                       ->delete($this->API_URL);
+                ->withToken($this->BEARER_TOKEN)
+                ->delete($this->API_URL);
         } catch (Exception $exception) {
             throwNgeniusPackageResponse($exception);
-        }catch(ConnectionException $connException){
+        } catch (ConnectionException $connException) {
             throwNgeniusPackageResponse($connException);
         }
     }
-
-
-
-
 }
